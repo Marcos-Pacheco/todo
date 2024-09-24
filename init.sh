8cec66edca29a972617a95161b34dcb96c7b1fd2
Marcos Pachêco <marcos.hr.pacheco@gmail.com>
2024-09-24 12:16:35 -0300

    🐛 fix: script inicialização do projeto

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 echo "============ CONFIGURANDO PROJETO, AGUARDE ... ============"
 echo ""
 
+docker compose up -d
 docker exec -it lumen bash -c 'composer install'
 docker exec -it postgres bash -c 'psql -U postgres -c "CREATE DATABASE todo;" || true'
 cp ./lumen/.env.example ./lumen/.env
